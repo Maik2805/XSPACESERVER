04cb15b71f82fdb5e32d2d354cefa5e197ea6a0c
Maik2805 <mscardenas77@misena.edu.co>
2024-06-16 16:56:38 -0500

    Add Lives Implementation

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
  * Module dependencies.
  */
 const { Server } = require("socket.io");
+const readline = require("readline");
 
 /**
  * Load environment variables from .env file.
@@ -25,13 +26,11 @@ const io = new Server({
 io.listen(port);
 
 io.on("connection", (socket) => {
-
   console.log(
     "Player joined with ID",
     socket.id,
     ". There are " + io.engine.clientsCount + " players connected."
   );
-
 
   socket.on("player-moving", (transforms) => {
     socket.broadcast.emit("player-moving", transforms);
@@ -40,8 +39,13 @@ io.on("connection", (socket) => {
   socket.on("player-shot", (transforms) => {
     socket.broadcast.emit("player-shot", transforms);
   });
+
   socket.on("player-dead", (data) => {
     socket.broadcast.emit("player-dead", data);
+  });
+
+  socket.on("current-lives", (data) => {
+    socket.broadcast.emit("current-lives", data);
   });
   socket.on("generate-live", (data) => {
     socket.broadcast.emit("generate-live", data);
@@ -56,13 +60,22 @@ io.on("connection", (socket) => {
   });
 });
 
+/**
+ * Set up readline interface to listen for keyboard input.
+ */
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// function createLive() {
-//   const clients = Array.from(io.sockets.sockets.values());
-//   if (clients.length > 0) {
-//     const randomIndex = Math.floor(Math.random() * clients.length);
-//     const randomSocket = clients[randomIndex];
-//     randomSocket.emit("generate-live", { message: "This is a live" });
-//   }
-// }
-// setInterval(createLive, 5000);
+rl.on('line', (input) => {
+  if (input.trim() === '1') {
+    // Emit the 'generate-live' event to all connected clients
+    io.emit("generate-live", { id: "projectileId", position: [2, 3, -45] });
+    console.log('Emitted "generate-live" event to all clients.');
+  } else if (input.trim() === '2') {
+    // Emit the 'generate-live' event to all connected clients
+    io.emit("current-lives", { lives: 4 });
+    console.log('current lives event to all clients.');
+  }
+});
